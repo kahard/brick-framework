204b2326b8c378d942eb31034dc4358fc851ff5d
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-15 17:54:08 +0200

    Ignore generated build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/work/brick_framework
-# Build directory: D:/work/brick_framework/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[brick_core_tests]=] "D:/work/brick_framework/build/brick_core_tests.exe")
-set_tests_properties([=[brick_core_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/work/brick_framework/CMakeLists.txt;29;add_test;D:/work/brick_framework/CMakeLists.txt;0;")

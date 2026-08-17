eb08615c2192f60749dacc258c88bbb925d926dd
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-17 07:06:43 +0200

    Add ESP8266 ST7789 and GPIO button adapters

---
+++ libs/interfaces/include/brick/interfaces/input/button.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace brick::interfaces::input {
+
+class IButton {
+ public:
+  virtual ~IButton() = default;
+
+  virtual bool begin() = 0;
+  virtual bool is_pressed() const = 0;
+};
+
+}  // namespace brick::interfaces::input

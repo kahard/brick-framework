9e6f22735bec633b344e361502e729794572f68a
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-22 11:35:30 +0200

    feat(esp32p4): add JC8012 touch and accelerated rotation

--- platforms/esp32/include/brick/platform/esp32/p4/MipiDsiDisplay.h
+++ platforms/esp32/include/brick/platform/esp32/p4/MipiDsiDisplay.h
@@ -9,6 +9,7 @@
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
 #include "esp_ldo_regulator.h"
+#include "driver/ppa.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
@@ -33,6 +34,7 @@ struct MipiDsiPanelConfig
     gpio_num_t    reset_gpio              = GPIO_NUM_NC;
     int           mipi_dsi_ldo_channel    = -1;
     int           mipi_dsi_ldo_voltage_mv = 0;
+    brick::interfaces::display::Rotation default_rotation = brick::interfaces::display::Rotation::rotate_0;
 
     // Sequence format: command, argument_count, arguments...
     // A command with argument_count 0xFF means delay command milliseconds.
@@ -57,6 +59,12 @@ private:
     bool apply_rotation_();
     static bool IRAM_ATTR on_color_trans_done_(esp_lcd_panel_handle_t, esp_lcd_dpi_panel_event_data_t*, void*);
     static bool IRAM_ATTR on_refresh_done_(esp_lcd_panel_handle_t, esp_lcd_dpi_panel_event_data_t*, void*);
+    bool rotated_transfer_(brick::interfaces::display::DisplayRect area,
+                           const brick::interfaces::display::PixelBuffer& buffer);
+    bool ensure_rotation_buffers_();
+    void release_rotation_buffers_();
+    void rotate_buffer_(const std::uint16_t* source);
+    bool ppa_rotate_buffer_(const std::uint16_t* source);
 
     MipiDsiPanelConfig                   config_;
     brick::interfaces::display::Rotation rotation_     = brick::interfaces::display::Rotation::rotate_0;
@@ -68,6 +76,12 @@ private:
     SemaphoreHandle_t                    refresh_done_ = nullptr;
     volatile bool                        color_ready_for_refresh_ = false;
     bool                                 initialized_  = false;
+    bool                                 transfer_pending_ = false;
+    std::uint16_t                        logical_width_ = 0;
+    std::uint16_t                        logical_height_ = 0;
+    std::uint16_t*                       rotation_source_ = nullptr;
+    std::uint16_t*                       rotation_target_ = nullptr;
+    ppa_client_handle_t                  ppa_client_ = nullptr;
 
     static constexpr const char* TAG = "brick_mipi_dsi";
 };
